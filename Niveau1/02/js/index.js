2b0a552961d122f63ec16277a7440dfac7d13ba1
Halsbrand1990 <46779000+Halsbrand1990@users.noreply.github.com>
2019-02-08 09:22:22 +0100

    final

--- Niveau1/02/js/index.js
+++ Niveau1/02/js/index.js
@@ -1,33 +1,32 @@
 document.addEventListener ("DOMContentLoaded", () => {
 
-let resultat = beregnForhold(4,3);
-console.log(resultat);
+	let resultat = beregnForhold(4,3);
+	console.log(resultat);
+
+	let resultat2 = beregnForhold2(4,0);
+	console.log(resultat2);
+
+	let resultat3 = beregnForhold2(4,5);
+	console.log(resultat3);
 
 
 
-let resultat2 = beregnForhold2(4,0);
-console.log(resultat2);
-
-function beregnForhold(a,b) {
+	function beregnForhold(a,b) {
 
 	return a/b;
 
-	// console.log(resultat);
-
-};
+	};
 
 
 
-function beregnForhold2(a,b) {
+	function beregnForhold2(a,b) {
 
 	if (b==0) {
 		return null;
-	}
+		}
 	else {
 		return a/b;
-	};
-	
-	console.log(beregnForhold2());
+		};
 	
 	};
 
