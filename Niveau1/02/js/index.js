76177f0f55641f81c36e0e0badc3ca30e1d3f08e
Halsbrand1990 <46779000+Halsbrand1990@users.noreply.github.com>
2019-02-11 08:23:36 +0100

    ny opgave

--- Niveau1/02/js/index.js
+++ Niveau1/02/js/index.js
@@ -10,22 +10,20 @@ document.addEventListener ("DOMContentLoaded", () => {
 	console.log(resultat3);
 
 
-
 	function beregnForhold(a,b) {
 
-	return a/b;
+		return a/b;
 
 	};
 
 
-
 	function beregnForhold2(a,b) {
 
-	if (b==0) {
-		return null;
-		}
-	else {
-		return a/b;
+		if (b==0) {
+			return null;
+			}
+		else {
+			return a/b;
 		};
 	
 	};
